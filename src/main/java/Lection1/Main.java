aac4b95d3f36a188cbbdad70fd352ec9051dfcf7
Denzel1337 <denis-viper2013@ya.ru>
2021-01-03 14:01:55 +0400

    Initial Commit. I've done first lecture. I'm also reading a book about Algoritmics and Data Structures, and I'll append tasks to corresponding foilder.

---
+++ src/main/java/Lection1/Main.java
@@ -0,0 +1,34 @@
+package Lection1;
+
+import java.util.Arrays;
+
+public class Main  {
+    private static String s = "Hello, world!";
+    private static String s2 = "hello, world!";
+    public static void main(String[] args) {
+        System.out.println(s.length());
+        System.out.println(s.equalsIgnoreCase(s2));
+        String s3 = "String in String Pool";
+        char[] chars = s.toCharArray();
+        byte[] bytes = s.getBytes();
+        System.out.println(s.toUpperCase());
+        System.out.println(s.indexOf("o"));
+        System.out.println(s.indexOf("a"));
+        System.out.println(s.lastIndexOf("o"));
+        System.out.println(s.lastIndexOf("a"));
+        System.out.println(s.contains("Sun"));
+        System.out.println(s.contains("wor"));
+        System.out.println(s.contains("world!"));
+        System.out.println(s.endsWith("Oracle"));
+        System.out.println(s.startsWith("Java"));
+        System.out.println(s.replaceAll("a", "o"));
+        System.out.println(s.replaceAll("o", "a"));
+        System.out.println(s.substring(4,8));
+        String[] arr = s.split(" ");
+        System.out.println(Arrays.toString(arr));
+        System.out.println(new StringBuffer(s).reverse());
+
+
+
+    }
+}
