c2ab26565e3fbe0e4e5089d6227d548258e0e239
Denis Mishin <Denis_Mishin@epam.com>
2021-01-11 21:27:08 +0400

    Add first method

--- src/main/java/SomeThing/MegaSuperClass.java
+++ src/main/java/SomeThing/MegaSuperClass.java
@@ -4,4 +4,7 @@ public class MegaSuperClass {
     public static void main(String[] args) {
         System.out.println("Это очень крутой класс,в котором реализована очень крутая фича, над которой будет вестись разработка, и которую надо вынсти в отдельный branch");
     }
+    public void coolMethod(){
+        System.out.println("Это первый крутой метод");
+    }
 }
